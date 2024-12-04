0279650003a8ff8a5fd2a9f354a967b5fda17436
Juan Pablo Hernandez Arias <ACjphernande@ads.loc>
2024-12-04 11:32:45 -0500

    Se agrega el proyecto

---
+++ js.js
@@ -0,0 +1,21 @@
+window.addEventListener("load",function(){
+    new Glider(this.document.querySelector(".carousel_lista"), {
+        slidesToShow: 5,
+        slidesToScroll: 2,
+        arrows: {
+            prev: '.carousel_izquierdo',
+            next: '.carousel_derecho'
+                }  
+    });
+});
+
+window.addEventListener("load",function(){
+    new Glider(this.document.querySelector(".carousel_lista0"), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: {
+            prev: '.carousel_izquierdo',
+            next: '.carousel_derecho'
+                }  
+    });
+});
